2748374530e10d77b5a14ef9cfc57ca32f0bd8fb
Jagadeshwar Gubba <Jagadeshwar Gubba@DESKTOP-SJG743S>
2021-02-10 12:07:05 +0530

    sample

---
+++ CurdOperationsInngJs/CurdOperationsInngJs/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CurdOperationsInngJs.MvcApplication" Language="C#" %>
---
+++ CurdOperationsInngJs/CurdOperationsInngJs/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CurdOperationsInngJs.Startup))]
+namespace CurdOperationsInngJs
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
